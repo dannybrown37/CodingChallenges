1ca45d5108b4c16803a92b7bfb8bb48bba32e70f
Danny Brown <dannybrown37@gmail.com>
2018-10-02 22:09:59 -0400

    couldn't resit tackling euler #6 tonight either

--- ProjectEuler/PyEuler.py
+++ ProjectEuler/PyEuler.py
@@ -1,5 +1,31 @@
 def main():
-    problem_5()
+    problem_6()
+
+
+def problem_6():
+    """
+    Sum square difference
+    Problem 6
+    The sum of the squares of the first ten natural numbers is,
+
+    12 + 22 + ... + 102 = 385
+    The square of the sum of the first ten natural numbers is,
+
+    (1 + 2 + ... + 10)2 = 552 = 3025
+    Hence the difference between the sum of the squares of the first ten
+    natural numbers and the square of the sum is 3025 - 385 = 2640.
+
+    Find the difference between the sum of the squares of the first one
+    hundred natural numbers and the square of the sum.
+    """
+    sum_of_squares = 0
+    square_of_sums = 0
+    for x in range(1, 101):
+        sum_of_squares += x**2
+        square_of_sums += x
+    square_of_sums = square_of_sums**2
+    answer = square_of_sums - sum_of_squares
+    print "Answer is %s" % answer
 
 
 def problem_5():
